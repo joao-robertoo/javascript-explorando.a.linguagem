7919987ca6257efce00ba21fb65cddf12c048d81
J <joao.adsbrasil@gmail.com>
2025-06-05 01:46:08 -0300

    Atribuição e imutabilidade

---
+++ 5-atribuicaoDeVariaveis.js
@@ -0,0 +1,15 @@
+console.log("Trabalhando com atribuição de variáveis");
+
+const idade = 29;
+const nome = "João";
+const sobrenome = "Roberto";
+
+console.log(nome + " " + sobrenome);
+console.log(nome, sobrenome);
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+let contador = 0;
+contador = contatdor + 1;
+const nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto);
+nomeCompleto = 2;
