b2a15a5b61b39b4737f6ed4a90d73192ddd32b8c
J <joao.adsbrasil@gmail.com>
2025-06-18 22:27:55 -0300

    Projeto final do curso

--- 8-loops.js
+++ 8-loops.js
@@ -1,31 +1,41 @@
 console.log(`\nTrabalhando com condicionais`);
-
 const listaDeDestinos = new Array(
     `Salvador`,
     `São Paulo`,
-    `Rio de Janeiro`,
-    `Curitiba`
+    `Rio de Janeiro`
 );
 
 const idadeComprador = 18;
 const estaAcompanhada = false;
 let temPassagemComprada = false;
-const destino = "Rio de Janeiro";
+const destino = "Curitiba";
 
 console.log("\n Destinos possíveis:");
-console.log(listaDedestinos);
+console.log(listaDeDestinos);
 
-const podeComprar = idadeComprador >= 18 || estaAcompanhada == true;
+const podeComprar  = idadeComprador >= 18 || estaAcompanhada == true;
 
 let contador = 0;
 let destinoExiste = false;
-while(contador<3) {
-    if (listaDeDestinos[contador] == destino) {
-        console.log("Destino existe");
+while(contador<3){
+    if(listaDeDestinos[contador] == destino){
+        destinoExiste = true;
         break;
-    } else {
-        console.log("Dstino não existe");
     }
-
     contador += 1;
+}
+
+console.log("Destino exite: ", destinoExiste);
+
+if(podeComprar && destinoExiste){
+    console.log("Boa Viagem");
+}else{
+    console.log("Desculpe tivemos um erro!");
+}
+
+for(let i  = 0 ; i < 3 ; i++){
+    if(listaDeDestinos[i] == destino){
+        destinoExiste = true;
+    }
+   
 }
