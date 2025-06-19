e5d41ff61c95c0f0cc2ea5e5fc5843394df7074e
J <joao.adsbrasil@gmail.com>
2025-06-18 22:11:35 -0300

    Break e Debbuger

--- 8-loops.js
+++ 8-loops.js
@@ -18,9 +18,11 @@ console.log(listaDedestinos);
 const podeComprar = idadeComprador >= 18 || estaAcompanhada == true;
 
 let contador = 0;
+let destinoExiste = false;
 while(contador<3) {
     if (listaDeDestinos[contador] == destino) {
         console.log("Destino existe");
+        break;
     } else {
         console.log("Dstino não existe");
     }
